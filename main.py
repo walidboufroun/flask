a77be3f1012844f4e1b1785d7701c5b00c8df137
Walid boufroune <81040703+walidboufroun@users.noreply.github.com>
2023-12-08 21:22:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def image():
     image = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body><h1>Hello World !</h1><form action="/resultat" method="post"><label for="file">Uploadd your pic</label><input type="file" name="file"><button type="submit">Valider</button></form></body></html>'
     return image 
 
-@app.route('/resultat')
+@app.route('/resultat', methods=['POST'])
 def resultat():
     if request.method == 'POST':
         input_data = request.form['file']
