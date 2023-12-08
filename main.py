4ed5513bb45fe40e7e9676e6cd15af78c92f52f8
Walid boufroune <81040703+walidboufroun@users.noreply.github.com>
2023-12-08 20:02:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    #return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
     return 'Welcome GNOV, Je suis un serveur gratuit avec python'
 
+@app.route('/image', methods=['POST'])
+def upload_image():
+    # Assuming you want to handle image uploads here
+    # Access the uploaded file using request.files
+    uploaded_file = request.files['file']
+    
+    # Process the uploaded file as needed
+    # You can save it, analyze it, etc.
+
+    return jsonify({"message": "Image uploaded successfully"})
+
+@app.route('/gnov-image', methods=['GET'])
+def get_gnov_image():
+    # Assuming you want to provide some image data in response
+    # You can customize this based on your requirements
+    image_data = {
+        "image_url": "https://example.com/gnov-image.jpg",
+        "description": "This is the GNOV image"
+    }
+
+    return jsonify(image_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
